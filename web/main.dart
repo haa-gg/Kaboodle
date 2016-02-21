a6d4abc13dc1e06c4808e22316d9a203376e9966
Davin Loegering <davin.loegering@cliftonlabs.com>
2016-02-20 23:41:32 -0600

    Fixed the pubspec typo.

--- web/main.dart
+++ web/main.dart
@@ -3,8 +3,7 @@ import 'dart:html';
 void loadData() {
   var url = "http://eve-marketdata.com/api/item_orders2.json?char_name=demo&buysell=b&type_ids=34,36";
 
-  // call the web server asynchronously
-  var request = HttpRequest.getString(url).then(onDataLoaded);
+  HttpRequest.getString(url).then((result) => print('Request complete $result'));
 }
 
 void onDataLoaded(String responseText) {
