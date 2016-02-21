83e340546c7ab6e2e8fd3a037c05760712bd8f68
Gigatweak <gigafry665@gmail.com>
2016-02-20 20:51:20 -0600

    Main dart file

---
+++ main.dart
@@ -0,0 +1,15 @@
+void loadData() {
+  var url = "http://eve-marketdata.com/api/item_orders2.json?char_name=demo&buysell=b&type_ids=34,36";
+
+  // call the web server asynchronously
+  var request = HttpRequest.getString(url).then(onDataLoaded);
+}
+
+void onDataLoaded(String responseText) {
+  var jsonString = responseText;
+  print(jsonString);
+}
+
+main() {
+  loadData();
+}
